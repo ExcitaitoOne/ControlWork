21a75ed423621d1447799cee06d3741931d68168
ExcitaitoOne <roja28@yandex.ru>
2022-11-27 18:05:24 +0700

    1 commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Свормировать массив из строк, и сделать отбор по 3 первым символам
+
+void FillArray(string [] massiv){
+    for(int i = 0; i < massiv.Length; i++){
+        
+    }
+
+}
+
+void PrintArray(string[] massiv){
+     for(int i = 0; i < massiv.Length; i++){
+        Console.WriteLine(" " + massiv[i]);
+    }
+}
+string[] Massiv = new string [5] {"asd" , "dddddd" ,"asddf", "asdq", "asdqe1"}; 
+
+PrintArray(Massiv);
